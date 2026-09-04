ea573cee6f208a924df8d35432457751679910b5
TallerM Admin <admin@tallerm.com>
2026-09-04 19:44:03 -0400

    deploy: modo visita + build web Mi Cafetal App

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "18862807"
+    serviceWorkerVersion: "3969886372"
   }
 });
