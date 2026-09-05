af339588cf209ffd176345d32b8e9540de9b83a6
TallerM Admin <admin@tallerm.com>
2026-09-05 14:43:26 -0400

    deploy: cuadro resumen jerarquico con resultado resaltado

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3940215164"
+    serviceWorkerVersion: "3655199052"
   }
 });
