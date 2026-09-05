4f5202cb31286017aed0e8f028e4d25b3a5e572f
TallerM Admin <admin@tallerm.com>
2026-09-05 11:05:16 -0400

    deploy: separar cultivo/categoria y ampliar catalogo para cualquier cultivo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2987517696"
+    serviceWorkerVersion: "3332886947"
   }
 });
