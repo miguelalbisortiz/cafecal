843f0c90616aeb0500b1dac7613cac7ef2a38b78
TallerM Admin <admin@tallerm.com>
2026-09-15 10:22:07 -0400

    deploy: fix onboarding bypass for new users

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "755753141"
+    serviceWorkerVersion: "3803140105"
   }
 });
