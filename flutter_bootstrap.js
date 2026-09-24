4679120b07def545328ac457b30cbd36816c7c5a
TallerM Admin <admin@tallerm.com>
2026-09-24 13:32:46 -0400

    deploy: menu sin Cosechas (oculto)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2950011437"
+    serviceWorkerVersion: "1159047911"
   }
 });
